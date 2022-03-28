e050560f533bcb8831bcfeb845725bc319161b68
crrs666 <fdjwd@foxmail.com>
2022-03-28 22:26:23 +0800

    a little better toolbar

--- app/src/main/java/com/ez/ezmonitor/HostEditActivity.java
+++ app/src/main/java/com/ez/ezmonitor/HostEditActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.ez.ezmonitor.databinding.ActivityEditHostBinding;
 
 public class HostEditActivity extends AppCompatActivity {
     private ActivityEditHostBinding binding;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,5 +18,11 @@ public class HostEditActivity extends AppCompatActivity {
 
         binding = ActivityEditHostBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setTitle("Edit Host");
     }
 }
